f78a144e2f9b2d9f43279b6a9edfd8186afeaa41
Natalia Wunder <nwunder14@gmail.com>
2025-07-16 10:19:14 -0400

    added request to flow functionality implemented in context menu

--- ExtensionTemplateProject/src/main/java/ui/ContextMenu.java
+++ ExtensionTemplateProject/src/main/java/ui/ContextMenu.java
@@ -1,9 +1,11 @@
 package ui;
 
 import burp.api.montoya.MontoyaApi;
-
+import burp.api.montoya.core.HighlightColor;
 import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
+import burp.api.montoya.http.message.HttpRequestResponse;
+import burp.api.montoya.http.message.requests.HttpRequest;
 import manager.FlowManager;
 
 import javax.swing.*;
@@ -26,8 +28,8 @@ public class ContextMenu implements ContextMenuItemsProvider {
 
         List<Component> menuItemList = new ArrayList<>();
 
+        // TO-DO: MAKE ADD NOTE FOR WHEN IT STARTS AND STOPS
         JMenuItem beginFlowItem = new JMenuItem("Begin Flow on Next Http");
-        // TODO: Implement listener to begin a flow
         beginFlowItem.addActionListener(l -> {
             String flowName = flowManager.createNextSequentialFlow();
             flowManager.setActiveFlow(flowName);
@@ -36,7 +38,6 @@ public class ContextMenu implements ContextMenuItemsProvider {
         menuItemList.add(beginFlowItem);
 
         JMenuItem stopFlowItem = new JMenuItem("Stop Current Flow");
-        // TODO: Implement listener to stop a flow
         stopFlowItem.addActionListener(l -> {
             flowManager.endCurrentFlow();
             montoyaApi.logging().logToOutput("Stopped current flow.");
@@ -44,12 +45,31 @@ public class ContextMenu implements ContextMenuItemsProvider {
         menuItemList.add(stopFlowItem);
 
         JMenu addRequestMenu = new JMenu("Add Request to Flow");
-        // TODO: Implement loop that creates JMenuItem for all flows available
-        
-        // JMenuItem menuItem3 = new JMenuItem("Add Request to Flow");
-        // // have logic to show new context menu 
-        // menuItem3.addActionListener(l -> {montoyaApi.logging().logToOutput("Hello, World.");});
-        // menuItemList.add(menuItem3);
+        JMenuItem createNewFlowItem = new JMenuItem("Create New Flow");
+        createNewFlowItem.addActionListener(l -> {
+            String newFlow = flowManager.createNextSequentialFlow();
+            List<HttpRequestResponse> selectedItems = event.selectedRequestResponses();
+            for (HttpRequestResponse item : selectedItems) {
+                flowManager.addRequestToFlow(newFlow, item.request());
+                item.annotations().setHighlightColor(HighlightColor.BLUE);
+            }
+            montoyaApi.logging().logToOutput("Created new flow and added " + selectedItems.size() + " request(s): " + newFlow);
+        });
+        addRequestMenu.add(createNewFlowItem);
+        addRequestMenu.addSeparator();
+
+        for (String flowName : flowManager.getAllFlows().keySet()){
+            JMenuItem flowItem = new JMenuItem(flowName);
+            flowItem.addActionListener(l -> {
+                List<HttpRequestResponse> selectedItems = event.selectedRequestResponses();
+                for (HttpRequestResponse item : selectedItems) {
+                    flowManager.addRequestToFlow(flowName, item.request());
+                    item.annotations().setHighlightColor(HighlightColor.BLUE);
+                }
+                montoyaApi.logging().logToOutput("Added " + selectedItems.size() + " request(s) to flow: " + flowName);
+            });
+            addRequestMenu.add(flowItem);
+        }
 
         menuItemList.add(addRequestMenu);
 
